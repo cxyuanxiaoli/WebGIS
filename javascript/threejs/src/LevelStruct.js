1e7e2c30bec1b8bcb024edc0037e5f3b56604525
lizhe <484016008@qq.com>
2025-12-04 23:45:43 +0800

    day37

---
+++ javascript/threejs/src/LevelStruct.js
@@ -0,0 +1,121 @@
+import { initScene } from "./common";
+import * as THREE from "three";
+
+const { scene, camera, renderer } = initScene();
+document.body.appendChild(renderer.domElement);
+
+//#region 组
+// 通过THREE.Group类创建一个组对象group,构建层级结构
+const lightGroup = new THREE.Group();
+const objectGroup = new THREE.Group();
+// 将组对象添加到场景中
+scene.add(lightGroup, objectGroup);
+
+// 场景对象Scene、组对象Group都有一个子对象属性
+console.log(scene.children);
+//#endregion
+
+//#region 光源
+const pointLight = new THREE.PointLight(0xffffff, 1, 1000, 0);
+pointLight.position.set(0, 50, 100);
+const ambientLight = new THREE.AmbientLight(0xcccccc, 0.2);
+lightGroup.add(pointLight, ambientLight);
+//#endregion
+
+//#region 物体
+const box = new THREE.Mesh(
+  new THREE.BoxGeometry(10, 10, 10),
+  new THREE.MeshLambertMaterial({ color: 0xff0000 })
+);
+box.position.set(0, 5, 0);
+const box2 = new THREE.Mesh(
+  new THREE.BoxGeometry(10, 10, 10),
+  new THREE.MeshLambertMaterial({ color: 0xff00ff })
+);
+box2.position.set(0, 20, 0);
+
+objectGroup.add(box, box2);
+
+// 父对象旋转缩放平移变换，子对象跟着变化
+objectGroup.rotation.y = Math.PI / 4;
+//#endregion
+
+//#region 遍历层级结构
+// 在层级模型中可以给一些模型对象通过.name属性命名进行标记
+const buildGroup = new THREE.Group();
+buildGroup.name = "建筑物组";
+scene.add(buildGroup);
+
+// 批量创建多个长方体表示高层楼
+const group1 = new THREE.Group(); //所有高层楼的父对象
+group1.name = "高层";
+for (let i = 0; i < 5; i++) {
+  const geometry = new THREE.BoxGeometry(20, 60, 10);
+  const material = new THREE.MeshLambertMaterial({
+    color: 0x00ffff,
+  });
+  const mesh = new THREE.Mesh(geometry, material);
+  mesh.position.x = i * 30; // 网格模型mesh沿着x轴方向阵列
+  group1.add(mesh); //添加到组对象group1
+  mesh.name = i + 1 + "号楼";
+  // console.log('mesh.name',mesh.name);
+}
+group1.position.set(0, 30, -50);
+
+const group2 = new THREE.Group();
+group2.name = "洋房";
+// 批量创建多个长方体表示洋房
+for (let i = 0; i < 5; i++) {
+  const geometry = new THREE.BoxGeometry(20, 30, 10);
+  const material = new THREE.MeshLambertMaterial({
+    color: 0x00ffff,
+  });
+  const mesh = new THREE.Mesh(geometry, material);
+  mesh.position.x = i * 30;
+  group2.add(mesh); //添加到组对象group2
+  mesh.name = i + 6 + "号楼";
+}
+group2.position.set(0, 15, -25);
+buildGroup.add(group1, group2); //添加到建筑物组buildGroup
+
+// 递归遍历方法.traverse()，可以遍历Threejs一个模型对象包含的所有后代
+buildGroup.traverse((obj) => {
+  console.log("所有模型节点的名称", obj.name);
+  // obj.isMesh：if判断模型对象obj是不是网格模型'Mesh'
+  if (obj.isMesh) {
+    //判断条件也可以是obj.type === 'Mesh'
+    obj.material.color.set(0xffff00);
+  }
+});
+
+// 查找某个具体的模型.getObjectByName()
+// Threejs和前端DOM一样，可以通过一个方法查找树结构父元素的某个后代对象
+// Threejs同样可以通过一些方法查找一个模型树中的某个节点
+const building2 = buildGroup.getObjectByName("2号楼");
+building2.material.color.set(0x00ff00);
+//#endregion
+
+//#region 本地坐标与世界坐标
+// 任何一个模型的本地坐标(局部坐标)就是模型的.position属性
+// 一个模型的世界坐标，是模型自身.position和所有父对象.position累加的坐标
+const testGroup = new THREE.Group();
+testGroup.position.set(30, -5, 30);
+const testBox = new THREE.Mesh(
+  new THREE.BoxGeometry(10, 10, 10),
+  new THREE.MeshLambertMaterial({ color: 0xaaaaaa })
+);
+testBox.position.set(20, 5, 20);
+testGroup.add(testBox);
+scene.add(testGroup);
+
+// .getWorldPosition(Vector3)读取一个模型的世界坐标，并把读取结果存储到参数Vector3中
+const worldPosition = new THREE.Vector3();
+testBox.getWorldPosition(worldPosition);
+console.log("世界坐标", worldPosition);
+console.log("本地坐标", testBox.position);
+
+//可视化局部坐标系
+const localAxesHelper = new THREE.AxesHelper(50);
+testGroup.add(localAxesHelper);
+
+//#endregion
--- javascript/threejs/src/Model.js
+++ javascript/threejs/src/Model.js
@@ -1,12 +1,206 @@
 import { initScene } from "./common";
 import * as THREE from "three";
 import { OrbitControls } from "three/addons/controls/OrbitControls.js";
+import { GLTFLoader } from "three/addons/loaders/GLTFLoader.js";
 
 const { scene, camera, renderer } = initScene();
+document.body.appendChild(renderer.domElement);
+console.log(scene);
 
+//#region 光源
+const ambientLight = new THREE.AmbientLight(0xffffff, 0.2);
+scene.add(ambientLight);
+
+const pointLight = new THREE.PointLight(0xffffff, 2, 1000);
+pointLight.decay = 0;
+pointLight.position.set(0, 100, 50);
+scene.add(pointLight);
+
+const pointLightHelper = new THREE.PointLightHelper(pointLight, 10, 0xff0000);
+scene.add(pointLightHelper);
+//#endregion
+
+//#region 控件
 const orbitControls = new OrbitControls(camera, renderer.domElement);
 
 const gridHelper = new THREE.GridHelper(100, 20);
 scene.add(gridHelper);
+//#endregion
 
-document.body.appendChild(renderer.domElement);
+//#region 模型加载
+const gltfLoader = new GLTFLoader();
+
+gltfLoader.load(
+  "../data/mytest.glb",
+  (data) => {
+    const model = data.scene;
+    // console.log(model);
+
+    model.scale.set(10, 10, 10);
+    // model.position.set(0,0,0);
+    scene.add(model);
+  },
+  undefined,
+  (error) => {
+    console.log("error loading model", error);
+  }
+);
+//#endregion
+
+//#region 三维向量Vector3
+const v = new THREE.Vector3(1, 1, 1);
+console.log(v);
+v.set(2, 2, 2);
+v.x = 3;
+console.log(v);
+
+// 三维向量Vector3有xyz三个分量，threejs中会用三维向量Vector3表示很多种数据
+// 如mesh.position、mesh.scale等属性值都是三维向量
+const mesh = new THREE.Mesh(
+  new THREE.BoxGeometry(10, 10, 10),
+  new THREE.MeshLambertMaterial({
+    color: 0xff0000,
+  })
+);
+scene.add(mesh);
+// 使用set方法设置位置
+mesh.position.set(0, 5, 20);
+
+// 执行.translateX()、.translateY()等方法本质上改变的都是模型的位置属性.position
+mesh.translateX(10);
+mesh.translateZ(-5);
+
+// 沿着自定义的方向移动，向量Vector3对象表示方向
+const axis = new THREE.Vector3(1, 0, 1);
+axis.normalize(); //向量归一化
+//沿着axis轴表示方向平移100
+mesh.translateOnAxis(axis, 10);
+
+// 使用set方法设置缩放
+mesh.scale.set(1.5, 1.5, 1.5);
+mesh.scale.y = 1;
+//#endregion
+
+//#region 欧拉对象Euler
+// mesh.rotation 是物体的角度属性，对应属性值是欧拉对象Euler
+const mesh2 = new THREE.Mesh(
+  new THREE.BoxGeometry(10, 10, 10),
+  new THREE.MeshLambertMaterial({
+    color: 0x00ff00,
+  })
+);
+scene.add(mesh2);
+// 使用set方法设置位置
+mesh2.position.set(-20, 5, 20);
+mesh2.rotateY(Math.PI / 2);
+console.log(mesh2.rotation);
+
+// 创建一个Euler对象
+const euler = new THREE.Euler(Math.PI / 6, Math.PI / 4, Math.PI / 3);
+console.log(euler);
+
+mesh2.rotation.x += Math.PI / 2;
+// 模型执行.rotateX()、.rotateY()等旋转方法，实质上改变了模型的角度属性
+mesh2.rotateZ(Math.PI / 2);
+
+const axis2 = new THREE.Vector3(0, 1, 0); //向量axis
+mesh2.rotateOnAxis(axis2, Math.PI / 8); //绕axis轴旋转π/8
+//#endregion
+
+//#region 颜色对象Color
+// threejs材质对象颜色属性.color是颜色对象Color
+const material = new THREE.MeshLambertMaterial(); //默认是纯白色0xffffff
+console.log(material.color);
+
+const sphereMesh = new THREE.Mesh(
+  new THREE.SphereGeometry(8, 32, 16),
+  material
+);
+sphereMesh.position.set(40, 8, 20);
+scene.add(sphereMesh);
+
+material.color = new THREE.Color("red");
+material.color.set(0x00ffff);
+material.color.setRGB(0.5, 0.5, 1);
+console.log(material);
+//#endregion
+
+//#region 模型的材质和几何体对象
+const mesh3 = new THREE.Mesh(
+  new THREE.BoxGeometry(10, 10, 10),
+  new THREE.MeshLambertMaterial()
+);
+console.log("geometry", mesh3.geometry, "material", mesh3.material);
+console.log(
+  "position",
+  mesh3.position,
+  "scale",
+  mesh3.scale,
+  "rotation",
+  mesh3.rotation
+);
+mesh3.position.set(20, 5, 40);
+scene.add(mesh3);
+
+const mesh4 = new THREE.Mesh(mesh3.geometry, mesh3.material);
+mesh4.position.set(40, 5, 40);
+scene.add(mesh4);
+mesh4.translateZ(5);
+
+// 两个mesh共享一个材质，改变一个mesh的颜色，另一个mesh2的颜色也会跟着改变
+mesh4.material.color.set(0xffff00);
+// 两个mesh共享一个几何体
+mesh4.geometry.translate(0, 0, 5);
+//#endregion
+
+//#region 克隆和复制
+// 克隆.clone()、复制.copy()是threejs很多对象都具有的方法
+// 比如三维向量对象Vector3、网格模型Mesh、几何体、材质
+const v1 = new THREE.Vector3(1, 2, 3);
+console.log("v1", v1);
+//v2是一个新的Vector3对象，和v1的.x、.y、.z属性值一样
+const v2 = v1.clone();
+console.log("v2", v2);
+console.log(v1 === v2);
+
+const v3 = new THREE.Vector3(1, 2, 3);
+const v4 = new THREE.Vector3(4, 5, 6);
+//读取v1.x、v1.y、v1.z的赋值给v3.x、v3.y、v3.z
+v4.copy(v3);
+console.log(v4);
+console.log(v3 === v4);
+
+const mesh5 = new THREE.Mesh(
+  new THREE.BoxGeometry(10, 10, 10),
+  new THREE.MeshLambertMaterial()
+);
+mesh5.position.set(-20, 5, 40);
+scene.add(mesh5);
+
+// 通过克隆.clone获得的新模型和原来的模型共享材质和几何体
+const mesh6 = mesh5.clone();
+console.log("mesh5", mesh5, "mesh6", mesh6);
+console.log(mesh5.position === mesh6.position);
+console.log(mesh5.material === mesh6.material);
+
+mesh6.position.set(-40, 5, 40);
+mesh6.material.color.set(0x0000ff);
+scene.add(mesh6);
+
+// 克隆几何体和材质，重新设置mesh的材质和几何体属性
+const mesh7 = mesh5.clone();
+mesh7.geometry = mesh5.geometry.clone();
+mesh7.material = mesh5.material.clone();
+mesh7.material.color.set(0xaa0000);
+mesh7.position.copy(mesh5.position);
+mesh7.position.y += 20;
+scene.add(mesh7);
+
+// 同步mesh5和mesh7的姿态角度
+function render() {
+  mesh5.rotation.x += 0.01;
+  mesh7.rotation.copy(mesh5.rotation);
+  requestAnimationFrame(render);
+}
+render();
+//#endregion
