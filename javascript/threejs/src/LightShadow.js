3f1cf349fcff841f12aadb439c1a6f7b31f76853
lizhe <484016008@qq.com>
2025-12-14 19:57:13 +0800

    day42

---
+++ javascript/threejs/src/LightShadow.js
@@ -0,0 +1,149 @@
+import { initScene } from "./common.js";
+import * as THREE from "three";
+import { GUI } from "dat.gui";
+
+const { scene, camera, renderer } = initScene();
+document.body.appendChild(renderer.domElement);
+
+renderer.setClearColor(0x666666);
+const gui = new GUI();
+const helperFolder = gui.addFolder("光源辅助");
+const boxFolder = gui.addFolder("阴影盒子");
+
+const ambientLight = new THREE.AmbientLight(0xffffff, 0.05);
+scene.add(ambientLight);
+gui.add(ambientLight, "intensity", 0, 0.2).name("环境光强度");
+
+//#region 聚光灯
+// 聚光源
+const spotLight = new THREE.SpotLight(0xffffff, 1.0, 230);
+scene.add(spotLight); //光源添加到场景中
+
+spotLight.angle = Math.PI / 4; //聚光灯角度
+spotLight.decay = 0.1; // 光源衰减
+spotLight.position.set(0, 200, 0); //聚光灯位置
+// spotLight.target是一个模型对象Object3D，默认在坐标原点
+console.log(spotLight.target);
+spotLight.target.position.set(30, 0, 30);
+//spotLight.target添加到场景中.target.position才会起作用
+scene.add(spotLight.target);
+gui.add(spotLight, "intensity", 0, 5).name("聚光灯强度");
+
+// 聚光源辅助对象，可视化聚光源
+const spotLightHelper = new THREE.SpotLightHelper(spotLight, 0xffffff);
+scene.add(spotLightHelper);
+helperFolder.add(spotLightHelper, "visible").name("聚光灯辅助");
+
+const boxMesh = new THREE.Mesh(
+  new THREE.BoxGeometry(50, 50, 50),
+  new THREE.MeshLambertMaterial({ color: 0xff0000 })
+);
+boxMesh.position.set(60, 25, 50);
+scene.add(boxMesh);
+//#endregion
+
+//#region 平行光阴影
+// 平行光DirectionalLight阴影步骤
+// 1..castShadow设置产生阴影的光源对象
+// 2..castShadow设置产生阴影的模型对象
+// 3..receiveShadow设置接收阴影效果的模型
+// 4..shadowMap.enabledWebGl渲染器允许阴影渲染
+// 5..shadow.camera设置光源阴影渲染范围
+
+const directLight = new THREE.DirectionalLight(0xffffff, 1.0);
+scene.add(directLight);
+directLight.position.set(-220, 190, 0);
+// 1.光源对象开启光源阴影计算
+directLight.castShadow = true;
+gui.add(directLight, "intensity", 0, 5).name("平行光强度");
+
+const directLightHelper = new THREE.DirectionalLightHelper(directLight, 10);
+scene.add(directLightHelper);
+helperFolder.add(directLightHelper, "visible").name("平行光辅助");
+
+const boxMesh2 = boxMesh.clone();
+boxMesh2.position.set(280, 25, 140);
+scene.add(boxMesh2);
+boxFolder.add(boxMesh2.position, "x", 100, 320);
+boxFolder.add(boxMesh2.position, "y", -20, 70);
+boxFolder.add(boxMesh2.position, "z", 40, 200);
+
+const planeMesh = new THREE.Mesh(
+  new THREE.PlaneGeometry(200, 200),
+  new THREE.MeshLambertMaterial({ color: 0x888888 })
+);
+planeMesh.position.set(300, 0, 130);
+planeMesh.rotation.x = -Math.PI / 2;
+scene.add(planeMesh);
+
+const boxMesh3 = boxMesh.clone();
+boxMesh3.position.set(350, 30, 110);
+scene.add(boxMesh3);
+boxMesh3.receiveShadow = true;
+boxMesh3.castShadow = true;
+
+// 2.产生阴影的模型对象开启阴影
+boxMesh2.castShadow = true;
+// 3.设置阴影的承载模型对象接收阴影
+planeMesh.receiveShadow = true;
+// 4.设置渲染器，允许光源阴影渲染
+renderer.shadowMap.enabled = true;
+//#endregion
+
+//#region 阴影范围.shadow.camera
+// 平行光DirectionalLight的.shadow属性是平行光阴影对象DirectionalLightShadow
+// 平行光阴影对象DirectionalLightShadow有一个相机属性.camera
+// .shadow.camera属性值(正投影相机OrthographicCamera)
+
+// 查看平行光阴影相机属性
+console.log("阴影相机属性", directLight.shadow.camera);
+// 5.设置三维场景计算阴影的范围
+directLight.shadow.camera.left = -50;
+directLight.shadow.camera.right = 250;
+directLight.shadow.camera.top = 300;
+directLight.shadow.camera.bottom = -50;
+directLight.shadow.camera.near = 0.5;
+directLight.shadow.camera.far = 600;
+
+// 可视化平行光阴影对应的正投影相机对象
+const cameraHelper = new THREE.CameraHelper(directLight.shadow.camera);
+scene.add(cameraHelper);
+helperFolder.add(cameraHelper, "visible").name("平行光阴影相机辅助");
+
+//#endregion
+
+//#region 阴影贴图尺寸.mapSize和阴影半径.radius
+// mapSize属性默认512x512
+// 在能覆盖包含阴影渲染范围的情况下，.shadow.camera的尺寸尽量小
+// 增加.shadow.camera长方体尺寸范围，阴影模糊锯齿感，可以提升.shadow.mapSize的大小
+console.log("阴影默认像素", directLight.shadow.mapSize);
+directLight.shadow.mapSize.set(1024, 1024);
+
+// 模糊弱化阴影边缘
+console.log(".shadow.radius", directLight.shadow.radius);
+directLight.shadow.radius = 2;
+//#endregion
+
+//#region 环境贴图
+
+const cubeTexture = new THREE.CubeTextureLoader().load([
+  "../data/env1.jpg",
+  "../data/env1.jpg",
+  "../data/env1.jpg",
+  "../data/env1.jpg",
+  "../data/env1.jpg",
+  "../data/env1.jpg",
+]);
+const box1 = new THREE.Mesh(
+  new THREE.BoxGeometry(30, 30, 30),
+  new THREE.MeshStandardMaterial({
+    // color: 0xff0000,
+    roughness: 0,
+    metalness: 1,
+    envMap: cubeTexture,
+  })
+);
+box1.position.set(20, 15, 350);
+scene.add(box1);
+
+//#endregion
